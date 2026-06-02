1bd2c522e5707a959290b6ca71044fd3b9fa719b
rodrigomgrassioto <rodrigomgrassioto@gmail.com>
2026-06-02 17:04:37 -0400

    test: Fazendo teste com valor estático

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,27 @@
-// src/main.ts
-import packageJson from "../package.json" with { type: "json" };
+// import packageJson from "../package.json" with { type: "json" };
+//
+// const mensagem: string = "Olá, mundo! Pokedex em TypeScript funcionando! 🚀";
+//
+// function exibirDados(texto: string, versao: string): void {
+//     console.log(`${texto} (v${versao})`);
+// }
+//
+// exibirDados(mensagem, packageJson.version);
 
-const mensagem: string = "Olá, mundo! Pokedex em TypeScript funcionando! 🚀";
+import { PokeApiService } from "./services/PokeApiService.js";
 
-function exibirDados(texto: string, versao: string): void {
-    console.log(`${texto} (v${versao})`);
+const apiService = new PokeApiService();
+
+async function rodarTeste() {
+    console.log("Buscando o Pokémon Pikachu...");
+    const pokemon = await apiService.buscarPokemon("Pikachu");
+
+    if (pokemon) {
+        console.log("Sucesso! Retorno mapeado:", pokemon);
+    } else {
+        console.log("Pokémon não encontrado.");
+    }
 }
 
-exibirDados(mensagem, packageJson.version);
+rodarTeste();
+
