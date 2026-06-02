30bdc6c4463857131c2d33ac854ad857b30a908b
rodrigomgrassioto <rodrigomgrassioto@gmail.com>
2026-06-02 15:51:29 -0400

    feat: Criado interface de retorno da API RF03

--- src/models/Pokemon.ts
+++ src/models/Pokemon.ts
@@ -1,7 +1,21 @@
+// RF02 – Criar uma interface para o Pokémon resumido - pg 6
 export interface PokemonResumo {
     id: number;
     nome: string;
     tipos: string[];
     altura: number;
     peso: number;
+}
+
+// RF03 – Criar uma interface para o retorno da API - pg 6 e 7
+export interface PokemonApiResponse {
+    id: number;
+    name: string;
+    height: number;
+    weight: number;
+    types: {
+        type: {
+            name: string;
+        };
+    }[];
 }
