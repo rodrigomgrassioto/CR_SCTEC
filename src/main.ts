3bfedc688e5d70217e51f8b2e496636fd673fa22
rodrigomgrassioto <rodrigomgrassioto@gmail.com>
2026-06-03 18:14:51 -0400

    test: Testando requisito funcional RF07.

--- src/main.ts
+++ src/main.ts
@@ -9,18 +9,26 @@
 // exibirDados(mensagem, packageJson.version);
 
 import { PokeApiService } from "./services/PokeApiService.js";
+import type {PokemonResumo} from "./models/Pokemon.js";
 
 const apiService = new PokeApiService();
 
 async function rodarTeste() {
+    let catalogo: PokemonResumo[] = [
+        {id: 25, 'nome': 'pikachu', altura: 4, peso: 60, tipos: ['electric']}
+    ];
     console.log("Buscando o Pokémon Pikachu...");
-    const pokemon = await apiService.buscarPokemon("Pikachu");
+    const pokemon = await apiService.buscarPokemon("26");
 
     if (pokemon) {
+        catalogo.push(pokemon);
         console.log("Sucesso! Retorno mapeado:", pokemon);
     // } else {
     //     console.log("Pokémon não encontrado.");
     }
+    console.log('Pokémons consultados');
+    console.log(pokemon)
+
 }
 
 rodarTeste();
