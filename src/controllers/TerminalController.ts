a63b8b2e1eda09b2243cdf011d4c733b39eb388e
rodrigomgrassioto <rodrigomgrassioto@gmail.com>
2026-06-05 11:20:04 -0400

    feat: Criado submenu adicionar e ajustado função para receber valores dinâmicos.

--- src/controllers/TerminalController.ts
+++ src/controllers/TerminalController.ts
@@ -5,7 +5,7 @@ import {Pokemon, type PokemonResumo} from "../models/Pokemon.js";
 export class TerminalController {
     private rl: readline.Interface;
     private catalogo: PokemonResumo[] = [
-        {id: 25, 'nome': 'pikachu', altura: 4, peso: 60, tipos: ['electric']}
+        // {id: 25, 'nome': 'pikachu', altura: 4, peso: 60, tipos: ['electric']}
     ];
 
     constructor(
@@ -27,7 +27,7 @@ export class TerminalController {
         let continuar = true;
 
         while (continuar) {
-            console.log('\n--- MENU DO CATÁLOGO POKÉMON ---');
+            console.log('\n🟦 --- MENU INICIAL POKÉMON --- 🟦');
             console.log('1. Adicionar Pokémon');
             console.log('2. Listar Pokémons');
             console.log('3. Remover Pokémons');
@@ -37,7 +37,9 @@ export class TerminalController {
 
             switch (opcao.toLowerCase()) {
                 case '1':
-                    console.log('\n[Adicionando novo Pokémon]');
+                    continuar = false;
+                    console.clear()
+                    // console.log('\n[Adicionando novo Pokémon]');
                     // this.buscarPokemonNaApiAddCatalogo();
                     this.subMenuAdicionar();
                     // console.log('adicionado com sucesso!');
@@ -53,49 +55,66 @@ export class TerminalController {
                     break;
 
                 case '3':
-                    console.log('\n[Remover]');
+                    continuar = false
+                    console.clear()
+                    console.log('\n 🟦 R E M O V E R !!! 🟦');
                     if (this.catalogo.length === 0) {
-                        console.log('O catálogo está vazio.');
+                        console.log('❌ O catálogo está vazio.');
+                        continuar = true;
                     } else {
                         console.table(this.catalogo);
                     }
                     break;
 
                 case 's':
-                    console.log('Saindo do programa... Até mais!');
+                    console.clear()
+                    console.log('👋 👋 👋  Até mais!');
                     continuar = false;
                     this.rl.close(); // Fecha a interface do terminal
                     break;
 
                 default:
-                    console.log('Opção inválida! Tente novamente.');
+                    console.clear()
+                    console.log('❌ Opção inválida! Tente novamente.');
                     break;
             }
         }
     }
 
     private async subMenuAdicionar(): Promise<void> {
-        console.log('\n--- ADICIONAR POKÉMON ---');
-        const termo = await this.perguntar('Digite o Nome ou ID: ');
+        console.log('\n 🟦 --- ADICIONAR POKÉMON --- 🟦');
+        const termo = await this.perguntar('❓ Nome ou ID para adicionar: ');
 
         if (!termo.trim()) {
             console.log('❌ Entrada não pode ser vazia!');
             return;
         }
         console.log(`Buscando "${termo}" na PokéAPI...`);
-        const retApi = this.buscarPokemonNaApiAddCatalogo(termo);
-        console.log(`O retorno da api é: ${retApi}`)
+        const retApi = await this.buscarPokemonNaApiAddCatalogo(termo);
+        if (!retApi) {
+            // console.clear();
+            console.log('❌ Erro ao consultar Pokémon, verifique o ID/Nome');
+            this.iniciarMenu()
+            return;
+        }
+        // console.clear();
+        console.log('✅ Pokémon adicionado ao catálogo');
+        this.iniciarMenu()
+        return;
 
     }
 
     private async buscarPokemonNaApiAddCatalogo(termo: string): Promise<boolean> {
-        console.log(`Buscando o Pokémon pelo ID/Nome ${termo}...`);
+        // console.log(`Buscando o Pokémon pelo ID/Nome ${termo}...`);
         const pokemon: PokemonResumo|null = await this.apiService.buscarPokemon(termo.toLowerCase().trim());
 
+        // console.log(pokemon)
+
         if (!pokemon) {
-            console.log('poke não encontrado')
+            // console.log('poke não encontrado')
             return false
         }
+        this.catalogo.push(pokemon);
         return true;
     }
 
