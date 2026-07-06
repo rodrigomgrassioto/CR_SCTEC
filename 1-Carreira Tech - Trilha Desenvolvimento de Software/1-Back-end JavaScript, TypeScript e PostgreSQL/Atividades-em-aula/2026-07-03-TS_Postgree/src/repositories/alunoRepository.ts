5cc2644a803b4af45f4d7d26111b3f185b7a5a4f
rodrigomgrassioto <rodrigomgrassioto@gmail.com>
2026-07-06 19:16:31 -0400

    feat: Criado outras funções para os alunos.

--- 1-Carreira Tech - Trilha Desenvolvimento de Software/1-Back-end JavaScript, TypeScript e PostgreSQL/Atividades-em-aula/2026-07-03-TS_Postgree/src/repositories/alunoRepository.ts
+++ 1-Carreira Tech - Trilha Desenvolvimento de Software/1-Back-end JavaScript, TypeScript e PostgreSQL/Atividades-em-aula/2026-07-03-TS_Postgree/src/repositories/alunoRepository.ts
@@ -1,6 +1,19 @@
 import {Aluno} from "../types/aluno";
 import {pool} from "../config/db";
 
+// query tipada
+export async function bucarTodosAlunos(): Promise<Aluno[] | null> {
+    const sql = `SELECT * FROM alunos`
+    const result =  await pool.query<Aluno>(sql);
+    return await result.rows; // tipo Aluno
+}
+
+export async function buscarAlunoPorEmail(email:string): Promise<Aluno | null> {
+    const sql = `SELECT * FROM alunos WHERE email = $1`;
+    const result = await pool.query<Aluno>(sql, [email]);
+    return result.rows[0];
+}
+
 export async function criarAluno(
     nome: string,email: string, curso: string):Promise<Aluno> {
     const sql = `
@@ -12,17 +25,24 @@ export async function criarAluno(
     return result.rows[0];
 }
 
-// query tipada
-export async function bucarTodosAlunos(): Promise<Aluno[] | null> {
-    const sql = `SELECT * FROM alunos`
-    const result =  await pool.query<Aluno>(sql);
-    return await result.rows; // tipo Aluno
+export async function atualizarAluno(
+    id: number, curso: string):Promise<Aluno | null> {
+    const sql = `
+        UPDATE alunos
+        SET curso = $1
+        WHERE ID = $2
+        RETURNING *`;
+
+    const result = await pool.query<Aluno>(sql, [curso, id]);
+    return result.rows[0] ?? null;
 }
 
+export async function deletarAluno(
+    id: number):Promise<boolean> {
+    const sql = `
+        DELETE FROM alunos
+        WHERE ID = $1`;
 
-export async function buscarAlunoPorEmail(email:string): Promise<Aluno | null> {
-    const sql = `SELECT * FROM alunos WHERE email = $1`;
-    const result = await pool.query<Aluno>(sql, [email]);
-    return result.rows[0];
-
+    const result = await pool.query<Aluno>(sql, [id]);
+    return (result.rowCount ?? 0) > 0;
 }
