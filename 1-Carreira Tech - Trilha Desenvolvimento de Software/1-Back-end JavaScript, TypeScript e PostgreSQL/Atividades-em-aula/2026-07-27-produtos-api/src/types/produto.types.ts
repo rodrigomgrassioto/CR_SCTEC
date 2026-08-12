fe6fa9387717cfb564ebdbb16f6bc2e7dff5440b
rodrigomgrassioto <rodrigomgrassioto@gmail.com>
2026-08-11 20:29:13 -0400

    feat: Cria model produto

---
+++ 1-Carreira Tech - Trilha Desenvolvimento de Software/1-Back-end JavaScript, TypeScript e PostgreSQL/Atividades-em-aula/2026-07-27-produtos-api/src/types/produto.types.ts
@@ -0,0 +1,27 @@
+// entidade produtos no banco de dados
+export interface Produto {
+    id: number;
+    nome: string;
+    preco: number
+    estoque: number;
+    ativo: boolean;
+    criadoEm: Date;
+}
+
+// DTO para criação
+export interface CreateProdutoDto {
+    nome: string;
+    preco: number
+    estoque: number;
+}
+
+// DTO para atualização - padrão PATCH agregando atributo ativo
+export type UpdateProdutoDto = Partial<CreateProdutoDto> & {ativo?: boolean}
+
+// Resposta paginada genérica
+export interface PaginatedResponse<T> {
+    data: T[];
+    total: number;
+    page: number;
+    limit: number;
+}
