06299f6bc4a18e0a915e0f222e41b472753e2822
rodrigomgrassioto <rodrigomgrassioto@gmail.com>
2026-08-12 10:54:52 -0400

    feat: Tipa request e response

--- 1-Carreira Tech - Trilha Desenvolvimento de Software/1-Back-end JavaScript, TypeScript e PostgreSQL/Atividades-em-aula/2026-07-27-produtos-api/src/routes/produtoRotes.ts
+++ 1-Carreira Tech - Trilha Desenvolvimento de Software/1-Back-end JavaScript, TypeScript e PostgreSQL/Atividades-em-aula/2026-07-27-produtos-api/src/routes/produtoRotes.ts
@@ -31,13 +31,20 @@ const produtosEmMemoria: Produto[] = [
     },
 ];
 // GET api/vi/produtos
-router.get('/', (req, res) => {
+/*
+Request<Params, ResBody, ReqBody, ReqQuery>
+1 {} = (Params): São os parâmetros de URL (como o :id). Como essa rota é apenas /, não tem parâmetros. Por isso está vazio {}
+2 Produtos[] = Essa segunda posição define o tipo do corpo da Resposta
+3 {} (ReqBody): Esse sim é o corpo da requisição (o que você envia). Como é um GET e não enviamos nada, ele deixou vazio
+4 {} (ReqQuery): São os parâmetros de busca que vão na URL depois da interrogação (ex: ?busca=cadeira).
+*/
+router.get('/', (req: Request<{}, Produto[], {}, {}>, res: Response<Produto[]>) => {
     res.json(produtosEmMemoria);
 })
 
 
 // GET api/vi/produtos/:id - busca por ID
-router.get('/:id', (req: Request, res: Response) => {
+router.get('/:id', (req: Request<{ id: string }>, res: Response<Produto | {error: string}>) => {
     const id = Number(req.params.id);
     if(isNaN(id)) return res.status(404).json({error: 'id deve ser número.' });
 
