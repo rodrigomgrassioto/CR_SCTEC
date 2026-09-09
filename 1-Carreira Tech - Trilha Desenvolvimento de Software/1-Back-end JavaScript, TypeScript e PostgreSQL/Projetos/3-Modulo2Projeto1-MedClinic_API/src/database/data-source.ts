883fa2073fcd1fe1af176f1f8c7aca92dd5e4aba
rodrigomgrassioto <rodrigomgrassioto@gmail.com>
2026-09-09 17:05:39 -0400

    feat: Executado spec 2_1

---
+++ 1-Carreira Tech - Trilha Desenvolvimento de Software/1-Back-end JavaScript, TypeScript e PostgreSQL/Projetos/3-Modulo2Projeto1-MedClinic_API/src/database/data-source.ts
@@ -0,0 +1,14 @@
+import { DataSource } from 'typeorm';
+
+export const AppDataSource = new DataSource({
+  type: "postgres",
+  host: process.env.DB_HOST || 'localhost',
+  port: Number(process.env.DB_PORT || 5432),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  synchronize: false, // Desativado para usar migrations com segurança - Se fosse true, o TypeORM mudaria as tabelas automaticamente e poderia apagar dados de produção
+  logging: true,      // Exibe as queries SQL no terminal para ajudar o dev júnior
+  entities: ["src/entities/*.ts", "dist/entities/*.js"],
+  migrations: ["src/database/migrations/*.ts", "dist/database/migrations/*.js"]
+});
